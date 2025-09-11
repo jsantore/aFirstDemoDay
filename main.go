87ed1f6b4cbeb36b8b53c31f1cc06dd077a48873
shinjitsu <shinjitsu@auxilioabalto.me>
2025-09-11 09:28:52 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"golang.org/x/image/colornames"
+)
+
+type firstGame struct {
+	player *ebiten.Image
+	xloc   int
+	yloc   int
+	score  int
+}
+
+func (f *firstGame) Update() error {
+	return nil
+}
+
+func (f *firstGame) Draw(screen *ebiten.Image) {
+	screen.Fill(colornames.Firebrick)
+}
+
+func (f *firstGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(1000, 1000)
+	ebiten.SetWindowTitle("First Class Example")
+	ourGame := firstGame{} //we will use the zero value for now
+	err := ebiten.RunGame(&ourGame)
+	if err != nil {
+		fmt.Println("Failed to run game", err)
+	}
+}
