1291f735ae8debd472790a712f65a3b41b86826f
shinjitsu <shinjitsu@auxilioabalto.me>
2025-09-11 09:59:57 -0400

    showing the ship

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	_ "image/png"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"golang.org/x/image/colornames"
 )
 
@@ -20,6 +22,10 @@ func (f firstGame) Update() error {
 
 func (f firstGame) Draw(screen *ebiten.Image) {
 	screen.Fill(colornames.Aqua)
+	drawOps := &ebiten.DrawImageOptions{}
+	drawOps.GeoM.Reset()
+	drawOps.GeoM.Translate(float64(f.xloc), float64(f.yloc))
+	screen.DrawImage(f.player, drawOps)
 }
 
 func (f firstGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -29,8 +35,13 @@ func (f firstGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenH
 func main() {
 	ebiten.SetWindowSize(1000, 1000)
 	ebiten.SetWindowTitle("First Class Example")
-	ourGame := firstGame{} //we will use the zero value for now
-	err := ebiten.RunGame(&ourGame)
+	playerImage, _, err := ebitenutil.NewImageFromFile("ship.png")
+	ourGame := firstGame{
+		player: playerImage,
+		xloc:   200,
+		yloc:   400,
+	} //we will use the zero value for now
+	err = ebiten.RunGame(&ourGame)
 	if err != nil {
 		fmt.Println("Failed to run game", err)
 	}
