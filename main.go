6b132e8075e2f3fd44df94f91120e7271dfd522e
shinjitsu <shinjitsu@auxilioabalto.me>
2025-09-11 10:48:46 -0400

    started coins but didn't finish it
    End of class thursday

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	_ "image/png"
+	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -10,17 +11,37 @@ import (
 )
 
 type firstGame struct {
-	player *ebiten.Image
-	xloc   int
-	yloc   int
-	score  int
+	player    *ebiten.Image
+	xloc      int
+	yloc      int
+	speed     int
+	score     int
+	treasures []coinPile
 }
 
-func (f firstGame) Update() error {
+type coinPile struct {
+	pict *ebiten.Image
+	xLoc float64
+	yLoc float64
+}
+
+func NewCoin(maxX, maxY int, image *ebiten.Image) *coinPile {
+	return &coinPile{
+		pict: image,
+		xLoc: float64(rand.Intn(maxX)),
+		yLoc: float64(rand.Intn(maxY)),
+	}
+}
+
+func (f *firstGame) Update() error {
+	f.xloc += f.speed
+	if f.xloc > (1000-f.player.Bounds().Dx()) || f.xloc < 0 {
+		f.speed = -f.speed
+	}
 	return nil
 }
 
-func (f firstGame) Draw(screen *ebiten.Image) {
+func (f *firstGame) Draw(screen *ebiten.Image) {
 	screen.Fill(colornames.Aqua)
 	drawOps := &ebiten.DrawImageOptions{}
 	drawOps.GeoM.Reset()
@@ -28,7 +49,7 @@ func (f firstGame) Draw(screen *ebiten.Image) {
 	screen.DrawImage(f.player, drawOps)
 }
 
-func (f firstGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+func (f *firstGame) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return outsideWidth, outsideHeight
 }
 
@@ -39,6 +60,7 @@ func main() {
 	ourGame := firstGame{
 		player: playerImage,
 		xloc:   200,
+		speed:  3,
 		yloc:   400,
 	} //we will use the zero value for now
 	err = ebiten.RunGame(&ourGame)
